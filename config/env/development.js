3579176497e268a2bed62745ee3caf76793d5def
Quentin MUSY <musy_q@epita.fr>
2017-08-25 19:16:15 +0200

    [ALL] prod env

--- config/env/development.js
+++ config/env/development.js
@@ -17,9 +17,6 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  //  models: {
-  //   connection: 'mysqlServer'
-  // }
   hookTimeout: 40000
 
 };
